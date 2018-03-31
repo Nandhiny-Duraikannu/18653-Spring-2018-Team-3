ccd6f5b44b55a2aaa7520f1e3b0c7544bb27dba3
Jérôme Kimbrough <jerome.kimbrough@edu.ece.fr>
2018-03-31 01:25:12 -0700

    Refactor everything in UI to scala except search API because it's going to disappear anyway -- add missing files

---
+++ backend/app/forms/MashupForm.java
@@ -0,0 +1,51 @@
+package forms;
+
+public class MashupForm {
+
+    private String name;
+    private String url;
+    private String description;
+    private String user;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Mashup{" +
+                "name='" + name + '\'' +
+                ", url='" + url + '\'' +
+                ", description='" + description + '\'' +
+                ", user='" + user + '\'' +
+                '}';
+    }
+}
