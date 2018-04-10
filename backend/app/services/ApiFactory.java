c3b8ff43993ae76b46d77beee3e18bdee7c69212
Tom Zhou <tomzhou94@gmail.com>
2018-04-10 11:27:49 -0700

    Fix bug in ApiFactory

--- backend/app/services/ApiFactory.java
+++ backend/app/services/ApiFactory.java
@@ -33,14 +33,4 @@ public class ApiFactory {
             return null;
         }
     }
-
-    public Follower followApi(Long user_id, Long api_id, Long submitter_id)
-        {
-            
-            Follower follower = new Follower();
-            follower.setParameters(user_id,api_id,submitter_id);
-            follower.save();
-            return follower;
-
-        }
 }
