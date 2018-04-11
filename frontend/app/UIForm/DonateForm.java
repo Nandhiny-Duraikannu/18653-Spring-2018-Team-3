9f064022591de0e93cda201432a5a9a607bc7815
sivaramakrishna7 <sivaramakrishna.k17@gmail.com>
2018-04-11 15:47:49 -0700

    Added Donate form

---
+++ frontend/app/UIForm/DonateForm.java
@@ -0,0 +1,50 @@
+package UIForm;
+
+public class DonateForm {
+
+    protected String amount;
+    protected String cardNumber;
+    protected String expiryDate;
+    protected String cvv;
+    protected String invoiceID;
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getInvoiceID() {
+        return invoiceID;
+    }
+
+    public void setInvoiceID(String invoiceID) {
+        this.invoiceID = invoiceID;
+    }
+}
